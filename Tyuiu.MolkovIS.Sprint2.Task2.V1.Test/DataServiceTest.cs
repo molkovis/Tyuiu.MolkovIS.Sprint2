e549eb87cf0ada6cd55390a1ee1a7642b734ad94
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-09-28 11:37:10 +0500

    Task2

---
+++ Tyuiu.MolkovIS.Sprint2.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.MolkovIS.Sprint2.Task2.V1.Lib;
+
+
+
+namespace Tyuiu.MolkovIS.Sprint2.Task2.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTEst
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService dataService = new DataService();
+            int x1 = 5;
+            int y1 = 5;
+
+            Assert.AreEqual(true, dataService.CheckDotInShadedArea(x1, y1));
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint2.Task2.V1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.MolkovIS.Sprint2.Task2.V1.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #2 | Выполнил: Мольков И. С.| ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт#02                                                               *");
+        Console.WriteLine("* Тема: Операторы сравнения                                               *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #29                                                             *");
+        Console.WriteLine("* Выполнил: Мольков Иван Сергеевич                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+        Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+        Console.WriteLine("***************************************************************************");
+        DataService dataService = new DataService();
+
+        Console.Write("Введите значение X: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите значение Y: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+
+
+        bool res = dataService.CheckDotInShadedArea(x, y);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(res);
+
+        Console.ReadKey();
+    }
+}
