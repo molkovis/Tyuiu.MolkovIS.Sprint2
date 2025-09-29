c0f4d108c98b3881cc8008c743d425857d1ffcfe
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-09-29 17:01:58 +0500

    Task6

---
+++ Tyuiu.MolkovIS.Sprint2.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MolkovIS.Sprint2.Task6.V5.Lib;
+
+
+namespace Tyuiu.MolkovIS.Sprint2.Task6.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService dataService = new DataService();
+
+            Assert.AreEqual("валет", dataService.FindCardValue(11));
+            Assert.AreEqual("десятка", dataService.FindCardValue(10));
+            Assert.AreEqual("шестерка", dataService.FindCardValue(6));
+            Assert.AreEqual("туз", dataService.FindCardValue(14));
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint2.Task6.V5/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MolkovIS.Sprint2.Task6.V5.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+        Console.WriteLine("***************************************************************************");
+        DataService dataService = new DataService();
+
+        Console.Write("Введите порядковое число: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        string res = "";
+
+        if (x >= 6 & x <= 14)
+        {
+            res = dataService.FindCardValue(x);
+        }
+        else
+        {
+            res = "Введено неверное значение";
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine($"Достоинство карты: {0}", res);
+
+        Console.ReadKey();
+    }
+}
